08019fa823860e353a15c04827ef75abfe40e9ce
khannedy <echo.khannedy@gmail.com>
2024-11-04 23:54:36 +0700

    Add support for updating user profile
    This update includes a new PATCH endpoint for /api/users/current, enabling users to update their name and password. Validation for the update request is also added, and corresponding tests are implemented to ensure proper functionality.

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,5 +1,5 @@
 import {Hono} from "hono";
-import {LoginUserRequest, RegisterUserRequest, toUserResponse} from "../model/user-model";
+import {LoginUserRequest, RegisterUserRequest, toUserResponse, UpdateUserRequest} from "../model/user-model";
 import {UserService} from "../service/user-service";
 import {ApplicationVariables} from "../model/app-model";
 import {User} from "@prisma/client";
@@ -40,5 +40,16 @@ userController.get('/api/users/current', async (c) => {
 
     return c.json({
         data: toUserResponse(user)
+    })
+})
+
+userController.patch('/api/users/current', async (c) => {
+    const user = c.get('user') as User
+    const request = await c.req.json() as UpdateUserRequest;
+
+    const response = await UserService.update(user, request)
+
+    return c.json({
+        data: response
     })
 })
--- src/validation/user-validation.ts
+++ src/validation/user-validation.ts
@@ -15,4 +15,9 @@ export class UserValidation {
 
     static readonly TOKEN: ZodType = z.string().min(1)
 
+    static readonly UPDATE: ZodType = z.object({
+        password: z.string().min(1).max(100).optional(),
+        name: z.string().min(1).max(100).optional(),
+    })
+
 }
